ef46527a68d5c4288c7fecc12a264ded9cb01a8f
tamrat-bayeh <tamrat3@gmail.com>
2020-04-23 12:51:45 +0300

    version update

--- server/client/src/Components/Profile/EditProfile.js
+++ server/client/src/Components/Profile/EditProfile.js
@@ -150,7 +150,6 @@ function EditProfile({ user, setedit, setuserData ,userData}) {
                         as="textarea"
                         rows="5"
                         onChange={saveInputData}
-                        }
                         required
                         name="about"
                         wrap="hard"
